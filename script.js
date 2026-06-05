ca64f56a712e7357e707fb84582e7b49680c9fad
Tornike Barbakadze <tbar3242@gmail.com>
2026-06-05 16:42:04 +0400

    last check

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // 1. function to get htlm element
 const fetchButton = document.getElementById('fetchDataBtn');
 const postsContainer = document.getElementById('postsContainer');
-
-
-
 // 2. Task 3, simple promise
 function checkServerStatus() {
     return new Promise(function(resolve, reject) {
-        let isServerOnline = true; // server 
-
+        let isServerOnline = true; // server
         if (isServerOnline) {
             resolve("კავშირი სერვერთან წარმატებულია!");
         } else {
@@ -16,26 +12,20 @@ function checkServerStatus() {
         }
     });
 }
-
 // 3. async/await and try/catch errors
 async function loadServerData() {
     postsContainer.innerHTML = "<p>იტვირთება...</p>";
-
     try {
         // Promise check
         const statusMessage = await checkServerStatus();
         console.log(statusMessage);
         // მივმართავთ რეალურ სერვერს პოსტების წამოსაღებად
         const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3');
-        
-        // 
         if (!response.ok) {
             throw new Error("მონაცემების წამოღება ვერ მოხერხდა.");
         }
-
         const posts = await response.json();
         postsContainer.innerHTML = ""; 
-
         // print posts 
         posts.forEach(function(post) {
             const postBlock = document.createElement('div');
@@ -47,12 +37,10 @@ async function loadServerData() {
             postBlock.innerHTML = "<h4>" + post.title + "</h4><p>" + post.body + "</p>";
             postsContainer.appendChild(postBlock);
         });
-
     } catch (error) {
         
         console.error("დაფიქსირდა შეცდომა:", error.message);
         postsContainer.innerHTML = "<p style='color: red;'>შეცდომა მონაცემების ჩატვირთვისას!</p>";
     }
 }
-
 fetchButton.addEventListener('click', loadServerData);
